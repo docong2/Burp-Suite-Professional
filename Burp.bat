879cceb030fdb25ad9c4f02751baf0cb20f5c37f
docong2 <155324402+docong2@users.noreply.github.com>
2024-06-12 20:50:18 +0700

    Create extensions.json

--- Burp.bat
+++ Burp.bat
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 "C:\Program Files\Java\jdk-21\bin\java.exe" "--add-opens=java.desktop/javax.swing=ALL-UNNAMED" "--add-opens=java.base/java.lang=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED" "-javaagent:BurpLoaderKeygen.jar" "-noverify" "-jar" "burpsuite_pro_v2024.4.5.jar"
+=======
+"C:\Program Files\Java\jdk-21\bin\java.exe" "--add-opens=java.desktop/javax.swing=ALL-UNNAMED" "--add-opens=java.base/java.lang=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED" "-javaagent:BurpLoaderKeygen.jar" "-noverify" "-jar" "burpsuite_pro_v2024.4.5.jar"
+>>>>>>> Stashed changes
